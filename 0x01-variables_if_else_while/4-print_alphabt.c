093340f9678d198557a52be2a78e23e0305fe089
ismail mamad <im179242@gmail.com>
2023-07-14 23:03:09 +0100

    Numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* return: Always 0 (Success)
+*main - Entry point
+*return: Always 0 (Success)
 */
 int main(void)
 {
-/**
-* main - Entry point
-* return: Always 0 (Success)
-*/
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {

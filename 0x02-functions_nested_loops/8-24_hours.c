fc8efbca12ee9453a628251623b8130da7db381e
ismail mamad <im179242@gmail.com>
2023-07-18 14:07:06 +0100

    Day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* jack_bauer -prints every minute of the day
+*
+* Return: Always 0.
+*/
+void jack_bauer(void)
+{
+int i, j, k, l;
+for (i = 0; i <= 2; i++)
+{
+for (j = 0; j <= 4; j++)
+{
+if (j == 4)
+continue;
+for (k = 0; k <= 6; k++)
+{
+if (k == 6)
+continue;
+for (l = 0; l <= 9; l++)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
+}
+}
+}
+}
+}

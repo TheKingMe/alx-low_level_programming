b58b85ba8b6c60148f7dff63c71bbb7643244b8a
ismail mamad <im179242@gmail.com>
2023-07-18 14:10:20 +0100

    Day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 int i, j, k, l;
 for (i = 0; i <= 2; i++)
 {
-for (j = 0; j <= 4; j++)
+for (j = 0; j <= 9; j++)
 {
-if (j == 4)
+if (j == 9)
 continue;
 for (k = 0; k <= 6; k++)
 {

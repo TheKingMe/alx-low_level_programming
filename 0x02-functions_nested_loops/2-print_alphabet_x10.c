941fb59cb78b2dc3df746cda55ba29699f381341
ismail mamad <im179242@gmail.com>
2023-07-18 12:11:08 +0100

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabet
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int i;
+char a;
+for (i = 0; i <= 10; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+i++;
+}
+}

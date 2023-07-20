5e9f252dead0fa0832389ee5581ef639b1c252fd
ismail mamad <im179242@gmail.com>
2023-07-20 14:26:21 +0100

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* _isupper - evalue if letter is uppercase .
+*_isupper - evalue if letter is uppercase .
 *@c: print int
-* Return: Always 0.
+*Return: Always 0.
 */
 int _isupper(char c)
 {

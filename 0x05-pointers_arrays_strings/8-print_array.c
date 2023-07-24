8f0d600bd4898f80e79730c29d754bcc1d2aa171
ismail mamad <im179242@gmail.com>
2023-07-24 16:31:18 +0100

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+#include <string.h>
+/**
+*print_array - array
+*@a: string
+*@n: number
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%c", s[i]);
+}
+}

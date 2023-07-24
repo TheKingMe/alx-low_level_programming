6c0a54376e755acd1d26f98659bc2048af11b588
ismail mamad <im179242@gmail.com>
2023-07-24 17:00:13 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,17 @@
 #include "main.h"
+/**
+*_strcpy - copy the string
+*@dest: past
+*@src: copy
+*Return: dest
+*/
 char *_strcpy(char *dest, char *src)
 {
-while (*dest++ = *src++);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
 return (dest);
 }

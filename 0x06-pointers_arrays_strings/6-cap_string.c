c90e29413fe0597249355c8ea5891864d7237cdf
ismail mamad <im179242@gmail.com>
2023-07-27 21:50:29 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,17 +10,35 @@ int i;
 for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i - 1] == ' ' || str[i - 1] == '\t')
-string_toupper(str[i]);
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
 if (str[i - 1] == ',' || str[i - 1] == ';')
-string_toupper(str[i]);
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
 if (str[i - 1] == '.' || str[i - 1] == '!')
-string_toupper(str[i]);
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
 if (str[i - 1] == '?' || str[i - 1] == '"')
-string_toupper(str[i]);
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
 if (str[i - 1] == '(' || str[i - 1] == ')')
-string_toupper(str[i]);
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
 if (str[i - 1] == '{' || str[i - 1] == '}')
-string_toupper(str[i]);
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
 }
 return (str);
 }

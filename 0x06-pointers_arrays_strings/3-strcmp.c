05861eca225b9bc60da60521bd14996a4b21c55f
ismail mamad <im179242@gmail.com>
2023-07-27 17:26:02 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strcmp - cmp two string
+*@s1:string 1
+*@s2:string 2
+*Return: str1 -
+*/
+int _strcmp(char *s1, char *s2)
+{
+char *str1 = s1;
+char *str2 = s2;
+while (*str1 != '\0' && *str2 != '\0' && *str1 == *str2)
+{
+str1++;
+st42++;
+}
+return (*str1 - *str2);
+}

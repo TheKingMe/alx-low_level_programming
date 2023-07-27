9e91e4631838592de8ed53941d281dfb46df47af
ismail mamad <im179242@gmail.com>
2023-07-27 01:00:13 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <string.h>
 /**
-*_strcayt - lcontract 2string
+*_strcat - lcontract 2string
 *@dest: string 2
 *@src: string 1
 *Return: dest
 */
 char *_strcat(char *dest, char *src)
 {
-int i, d = strlen(dest), s = strlen(src);
+int i, j, d = strlen(dest), s = strlen(src);
 for (i = d, j = 0; i < d + s, j < s; i++, j++)
 {
 dest[i] = src[j];

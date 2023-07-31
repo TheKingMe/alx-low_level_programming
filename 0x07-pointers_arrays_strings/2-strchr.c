9a3a41a4ea5ec23d5d04b0518a3b856375334dc4
ismail mamad <im179242@gmail.com>
2023-07-31 14:48:20 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "main.h"
 /**
 *_strchr - that locates a character in a string
@@ -9,7 +10,7 @@ char *_strchr(char *s, char c)
 {
 while (*s++)
 {
-if (s == c)
+if (*s == c)
 return (s);
 }
 return (NULL);

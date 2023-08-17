277358ea2318448c51cff6567b21f28da4085b62
ismail mamad <im179242@gmail.com>
2023-08-17 01:18:29 +0100

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
 * int_index - earches for an integer
-* @array: array to search in 
+* @array: array to search in
 * @size: size of the array
 * @cmp: pointer to the comparing function *
 * Return: index of the first element for which

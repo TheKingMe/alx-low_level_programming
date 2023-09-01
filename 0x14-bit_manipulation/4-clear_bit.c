6364b461abd3d0e23a304e3245666483eb2dff83
Ismail <im179242@gmail.com>
2023-09-01 01:14:13 +0100

    100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 /**
  * clear_bit -  that sets the value of a bit to 0 at a given index
  * @n: pointer to decimal
- * @index: index position 
+ * @index: index position
  * Return: 1 or -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)

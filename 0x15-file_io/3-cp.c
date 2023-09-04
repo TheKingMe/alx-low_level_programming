94c968991de55e52a85fe2829785b26de78b88de
Ismail <im179242@gmail.com>
2023-09-04 23:51:35 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,8 +21,9 @@ if (file_from == -1)
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 }
-file_to = open(argv[2], O_WRONLY | O_CREAT
-| S_IRGRP | S_IWGRP | S_IROTH | O_TRUNC, S_IRUSR | S_IWUSR);
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+if (file_to == -1)
 if (file_to == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);

dc33e7fa657ad22b753f7db3bdaa8e7c05966ce5
Ismail <im179242@gmail.com>
2023-10-12 23:52:33 +0100

    Print list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,4 @@
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
@@ -20,6 +21,5 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
 #endif

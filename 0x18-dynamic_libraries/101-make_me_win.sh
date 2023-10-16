140c1391b92974e726d8b7349dc235b643e79a19
Ismail <im179242@gmail.com>
2023-10-16 22:38:08 +0100

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget https://github.com/TheKingMe/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
+wget https://github.com/TheKingMe/alx-low_level_programming/raw
+/main/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
